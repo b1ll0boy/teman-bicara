d04a33275b75962eab2b8e00d2e9a8e5022a98e2
Blublug <valentino.banyubiru.vbb@gmail.com>
2023-09-15 22:17:49 +0700

    init

---
+++ app/src/main/java/com/example/temanbicara/ui/HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.temanbicara.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.temanbicara.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
