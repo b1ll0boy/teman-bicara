00eedcfd2cb0ef2246f6d9b3b0693dc6e901d30d
Wylie Raditya <wylie.raditya@gmail.com>
2023-10-19 16:34:07 +0700

    connect firebase

--- app/src/main/java/com/example/temanbicara/ui/LoginPage.kt
+++ app/src/main/java/com/example/temanbicara/ui/LoginPage.kt
@@ -7,25 +7,54 @@ import android.widget.TextView
 import com.example.temanbicara.R
 import android.text.method.LinkMovementMethod
 import android.widget.Button
+import android.widget.Toast
+import com.example.temanbicara.databinding.ActivityLoginPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginPageBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textClick = findViewById<TextView>(R.id.buat_akun)
-        textClick.setOnClickListener{
+//        val textClick = findViewById<TextView>(R.id.buat_akun)
+//        textClick.setOnClickListener{
+//            val intent = Intent(this, RegisterPage::class.java)
+//            startActivity(intent)
+//            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+//        }
+
+//        val buttonClick = findViewById<Button>(R.id.login)
+//        buttonClick.setOnClickListener {
+//            val intent = Intent(this, HomePage::class.java)
+//            startActivity(intent)
+//            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+//        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.buatAkun.setOnClickListener{
             val intent = Intent(this, RegisterPage::class.java)
             startActivity(intent)
-            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
-
         }
 
-        val buttonClick = findViewById<Button>(R.id.login)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, HomePage::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+        binding.login.setOnClickListener{
+            val email = binding.inputEmail.text.toString()
+            val password = binding.inputPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener() {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this@LoginPage, HomePage::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty fields Are not Allowed!!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/temanbicara/ui/RegisterPage.kt
+++ app/src/main/java/com/example/temanbicara/ui/RegisterPage.kt
@@ -5,18 +5,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.temanbicara.R
+import com.example.temanbicara.databinding.ActivityRegisterPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterPage : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterPageBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_page)
+        binding = ActivityRegisterPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textClick = findViewById<Button>(R.id.register)
-        textClick.setOnClickListener {
-            val intent = Intent(this, LoginPage::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+//        val textClick = findViewById<Button>(R.id.registerButton)
+//        textClick.setOnClickListener {
+//            val intent = Intent(this, LoginPage::class.java)
+//            startActivity(intent)
+//            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+//        }
+
+        binding.registerButton.setOnClickListener{
+            val name = binding.inputNama.text.toString()
+            val email = binding.inputEmail.text.toString()
+            val password = binding.inputPassword.text.toString()
+            val confirmPassword = binding.confirmPassword.text.toString()
+            val phone = binding.inputTelepon.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                if (password == confirmPassword) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener() {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this@RegisterPage, LoginPage::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty fields Are not Allowed!!", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
